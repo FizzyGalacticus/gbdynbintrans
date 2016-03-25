c7a66f3c0d18ccdfca7b2cf4ad9cefea2ad43525
Dustin L. Dodson <dustinleedodson@gmail.com>
2016-03-24 20:46:25 -0800

    Once again changed deliminating string for ROM.

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -63,7 +63,7 @@ void MainWindow::loadROM(QString filename) {
         if((int)data < 16)
             programText += "0";
         programText += QString::number((int)data, 16);
-        if(programText.contains("c32f027d"))
+        if(programText.contains("ffffffff"))
             break;
     }
     inFile.close();
